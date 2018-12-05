7586a93afaa7d02ec2405d1200be32b0b6058691
Maxim <maxim.marchal@hotmail.com>
2018-12-05 15:25:46 +0100

    Todos added

--- javascript/main.js
+++ javascript/main.js
@@ -7,6 +7,11 @@ var main = function(){
     
     // Place the ships in a space where you can drag them around.
     
+    // Add a listener to the randomize button --> pressing it should randomize the ships' locations
+
+
+    // Somehow make the ships draggable
+    
 
 };
 
