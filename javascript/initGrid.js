3ed4ae926793fb9d6082d1fc0345149630c652e0
Maxim <maxim.marchal@hotmail.com>
2018-12-05 15:25:19 +0100

    Added .js code that initializes the grid using a table

---
+++ javascript/initGrid.js
@@ -0,0 +1,24 @@
+ function  initGrid(){  
+    // Generate a 10 by 10 grid in the <span> <table> element
+    var w = 10, h = 10;
+    var $tableRef = $(".gridTable table");
+    var $tdToAdd;
+    var $trToAdd;
+    for(let i = 0; i<h; i++){
+        $trToAdd = $("<tr>");
+        for(let j = 0; j<w; j++){
+            // Add <td>'s with the proper ID to the <tr> elements.
+            $tdToAdd = $("<td>").attr("id",i+"COMMA"+j); // jQuery doesn't work when there's a comma in the id attribute.
+            $tdToAdd.append($("<div>")); // embed a <div> in the <td> 
+            $trToAdd.append($tdToAdd);
+        }
+        $tableRef.append($trToAdd);
+    }
+
+    // Return a function:
+        // Input: x,y coordinates
+        // Output: a jQuery object reference to Cell(x,y)
+    return function(x,y){
+        return $(x+"COMMA"+y);
+    }
+}
