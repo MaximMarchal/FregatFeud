a70a247fdd8a68b32a03089fc6b9bdfd8d5f88cc
Maxim <maxim.marchal@hotmail.com>
2018-12-05 19:13:13 +0100

    server side dingen

--- Vagrant/app/server.js
+++ Vagrant/app/server.js
@@ -9,6 +9,23 @@ console.log("server online at :3000");
 
 var gameHistory = [];
 
+var playerQueue = [];  // Playerqueue is an array of player IDs that are not playing right now
+app.post("/requestMatchmaking", function(req,res){
+    console.log("Player requested matchmaking!");
+    console.log("Player id: "+JSON.stringify(req.body));
+    var response = {
+        "title": "You have been queued up",
+        };
+    if(playerQueue.length > 0){
+        var opponentID = playerQueue.pop();
+        response["opponent_id"] = "p"+guidGenerator();
+        response["game_id"] = "g"+guidGenerator();
+    }
+    
+    
+    res.send(response);
+});
+
 app.get("/submitTurn", function(req, res){
     gameHistory.push(req); 
 
@@ -18,8 +35,10 @@ app.get("/submitTurn", function(req, res){
     res.send("submitTurn request received");
 });
 
-app.post("/requestMatchmaking", function(req,res){
-    console.log("Player requested matchmaking!");
-    console.log("Player id: "+req.body);
-    res.send("Message received");
-});
+function guidGenerator() {
+    // From: https://stackoverflow.com/questions/6860853/generate-random-string-for-div-id
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
