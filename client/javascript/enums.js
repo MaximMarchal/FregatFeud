9952cd9cd91e2d339e649c40776aa1dbeda71554
Maxim <maxim.marchal@hotmail.com>
2018-12-08 20:05:54 +0100

    verschillende message_types gedefinieerd

---
+++ client/javascript/enums.js
@@ -0,0 +1,19 @@
+const msg_types = {
+    // Server messages:
+    REQUEST_SHIP_LOCATION: 15,
+    PLAYER_WON: 2,
+    PLAYER_LOST: 3,
+    SHOT_FROM_OPPONENT: 14,
+    ENQUEUED: 192,
+    REQUEST_ID: 10101,
+
+    // Client messages:
+    ID_PAYLOAD: 9,
+    SEND_SHOT: 1,
+    SHIPS_LOCATION: 1337,
+
+    // Shared messages
+    OK: 777
+};
+
+
